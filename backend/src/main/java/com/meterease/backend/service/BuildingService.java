1a372e0d86158efd7a667c624a542f2cada0da21
ApLiesC <apisit.liesmann@gmail.com>
2026-07-12 01:02:45 +0700

    feat: implement building CRUD operations

--- backend/src/main/java/com/meterease/backend/service/BuildingService.java
+++ backend/src/main/java/com/meterease/backend/service/BuildingService.java
@@ -1,5 +1,16 @@
 package com.meterease.backend.service;
 
-public class BuildingService {
+import com.meterease.backend.dto.BuildingDTO;
 
+import java.util.List;
+
+public interface BuildingService {
+
+    BuildingDTO createBuilding(BuildingDTO buildingDTO);
+
+    List<BuildingDTO> getBuildings();
+
+    BuildingDTO updateBuilding(Integer buildingId, BuildingDTO buildingDTO);
+    
+    void deleteBuilding(Integer buildingId);
 }
