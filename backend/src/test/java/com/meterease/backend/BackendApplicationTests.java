0da5111848e0f9b2ca657cc0aa970e979218d89d
ApLiesC <apisit.liesmann@gmail.com>
2026-07-11 21:45:58 +0700

    chore: set up Spring Boot backend

---
+++ backend/src/test/java/com/meterease/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meterease.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
