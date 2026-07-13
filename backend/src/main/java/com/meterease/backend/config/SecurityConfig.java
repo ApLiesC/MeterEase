0b0d2146b23971fa6a4db7a77dc8601d4d5b74d4
Drwby <kevin_b@cmu.ac.th>
2026-07-14 01:58:45 +0700

    chore: Enable CORS

--- backend/src/main/java/com/meterease/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/meterease/backend/config/SecurityConfig.java
@@ -19,7 +19,13 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
 @Configuration
 public class SecurityConfig {
 
@@ -56,21 +62,64 @@ public class SecurityConfig {
             throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .cors(cors -> cors.configurationSource(
+                corsConfigurationSource()
+        ))
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session
+                .sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
                 )
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/api/auth/register",
-                                "/api/auth/login"
-                        ).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
-                );
+        )
+        .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/api/auth/register",
+                        "/api/auth/login"
+                ).permitAll()
+                .anyRequest().authenticated()
+        )
+        .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(Customizer.withDefaults())
+        );
 
         return http.build();
     }
+
+    @Bean
+public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(
+            List.of("http://localhost:5173")
+    );
+
+    configuration.setAllowedMethods(
+            List.of(
+                    "GET",
+                    "POST",
+                    "PUT",
+                    "DELETE",
+                    "OPTIONS"
+            )
+    );
+
+    configuration.setAllowedHeaders(
+            List.of(
+                    "Authorization",
+                    "Content-Type"
+            )
+    );
+
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration(
+            "/**",
+            configuration
+    );
+
+    return source;
+}
 }
